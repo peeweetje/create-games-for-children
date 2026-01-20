7c2d087753ec54499e2f895a1f904dfb0cc7ba17
PEEWEE\petra <petravos70@gmail.com>
2026-01-20 10:46:07 +0100

    Refactor game info js file into ts file.

--- src/components/GameInfo.tsx
+++ src/components/GameInfo.tsx
@@ -1,6 +1,13 @@
 import { motion } from 'framer-motion';
 
-export function GameInfo({ turn, isGameOver, isCheckmate, isDraw }) {
+interface GameInfoProps {
+    turn: 'w' | 'b';
+    isGameOver: boolean;
+    isCheckmate: boolean;
+    isDraw: boolean;
+}
+
+export function GameInfo({ turn, isGameOver, isCheckmate, isDraw }: GameInfoProps) {
     return (
         <div className="flex flex-col items-center justify-center p-4 bg-white rounded-xl shadow-lg border-2 border-orange-200">
             {isGameOver ? (
