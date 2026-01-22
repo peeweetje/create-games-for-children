546b7a8305349ddc7683bbca07b5e6b64a1e4d5c
PEEWEE\petra <petravos70@gmail.com>
2026-01-22 11:34:19 +0100

    update configs.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-        "./index.html",
-        "./src/**/*.{js,ts,jsx,tsx}",
-    ],
-    theme: {
-        extend: {
-            colors: {
-                'chess-light': '#f0d9b5',
-                'chess-dark': '#b58863',
-            },
-            fontFamily: {
-                'sans': ['"Comic Sans MS"', 'cursive', 'sans-serif'], // Child friendly default
-            }
-        },
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        'chess-light': '#f0d9b5',
+        'chess-dark': '#b58863',
+      },
+      fontFamily: {
+        sans: ['"Comic Sans MS"', 'cursive', 'sans-serif'],
+      },
     },
-    plugins: [],
+  },
 }
