d028078965fe3fa9c82cd1aa5f3b3c19fd5ca027
PEEWEE\petra <petravos70@gmail.com>
2026-01-28 11:54:08 +0100

    Add file for the layout.

---
+++ src/components/Layout.tsx
@@ -0,0 +1,13 @@
+import { Outlet } from 'react-router-dom';
+import { Sidebar } from './Sidebar';
+
+export const Layout = () => {
+    return (
+        <div className="flex min-h-screen bg-orange-50 font-sans text-gray-900">
+            <Sidebar />
+            <div className="flex-1 md:ml-48 lg:ml-64 w-full">
+                <Outlet />
+            </div>
+        </div>
+    );
+};
