93f56f07d557752b4c0689870825cfe91fcf589a
peeweetje <petravos70@gmail.com>
2026-07-23 08:47:45 +0200

    UPdate the semantic and add a11y to the component.

--- src/components/reading-page/ReadingHeader.tsx
+++ src/components/reading-page/ReadingHeader.tsx
@@ -6,13 +6,11 @@ export const ReadingHeader = () => {
     
     return (
         <div className="text-center space-y-2">
-            <div className="flex items-center justify-center gap-3">
-                <h1 className="text-2xl md:text-3xl font-extrabold text-primary-600 flex items-center gap-3 drop-shadow-sm tracking-wide">
-                    <div className="text-4xl">🔤</div>
-                    <div className="text-4xl">📖</div>
-                    {t("learn.reading.title")}
-                </h1>
-            </div>
+            <h1 className="text-2xl md:text-3xl font-extrabold text-primary-600 flex items-center justify-center gap-3 drop-shadow-sm tracking-wide">
+                <span className="text-4xl" aria-hidden="true">🔤</span>
+                <span className="text-4xl" aria-hidden="true">📖</span>
+                {t("learn.reading.title")}
+            </h1>
             <p className="p-2 text-xl text-text-600">
                 {t("learn.reading.subtitle")}
             </p>
